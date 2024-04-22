94f1289a253c535acc120711a3635150624a29dc
Max Hrytsiuk <maxeemja.inc@gmail.com>
2024-04-22 11:25:07 +0300

    url for images upd

--- src/components/Places.jsx
+++ src/components/Places.jsx
@@ -19,7 +19,7 @@ export default function Places({
 						<li key={place.id} className='place-item'>
 							<button onClick={() => onSelectPlace(place)}>
 								<img
-									src={`http://localhost:3000/${place.image.src}`}
+									src={`https://react-placepicker.onrender.com/${place.image.src}`}
 									alt={place.image.alt}
 								/>
 								<h3>{place.title}</h3>
